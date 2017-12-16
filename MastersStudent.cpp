be71ab8c306c962317569cfcbcdaadabc5994783
jkurian49 <jasonkurian@Jasons-MacBook-Air.local>
2017-12-15 19:42:00 -0500

    HW06

--- MastersStudent.cpp
+++ MastersStudent.cpp
@@ -1,4 +1,6 @@
 #include "MastersStudent.h"
+#include <iostream>
+#include <iomanip>
 
 // Constructor Implementations
 MastersStudent::MastersStudent(Student ug, int msy):
@@ -10,16 +12,22 @@ MastersStudent::MastersStudent(std::string fn, std::string ln,
 
 // New methods specific to MastersStudent
 float MastersStudent::getMsGpa() {
-  // TODO Implement this 
-  return 0.0;
+  	float sum = 0;
+	for (int i = 0; i <_ms_grades.size();i++) {
+		sum = _ms_grades[i] + sum;
+	}
+	return sum / _ms_grades.size();
 }
 
 void MastersStudent::addMsGrade(float grade) {
-  // TODO Implement this 
+   _ms_grades.push_back(grade);
+	return;
 }
 
 // Override Student's printInfo to include new fields
 void MastersStudent::printInfo() {
-  Student::printInfo();
-  // TODO Print additional MastersStudent fields
+  Student::printInfo(); {
+	std::cout << "MS " << majorString(_major) << ": " << _ms_grad_year << std::endl;
+	std::cout << "MS GPA: " << std::fixed << std::setprecision(2) << getMsGpa() << std::endl;
+  }
 }
--- Student.cpp
+++ Student.cpp
@@ -1,5 +1,9 @@
-#include <stdio.h>
 #include "Student.h"
+#include <string>
+#include <stdio.h>
+#include <iostream>
+#include <iomanip>
+#include <vector>
 
 Student::Student(std::string fn, std::string ln, int y, Major m)
   : _first_name(fn), _last_name(ln), _grad_year(y), _major(m) {}
@@ -7,20 +11,52 @@ Student::Student(std::string fn, std::string ln, int y, Major m)
 Student::~Student() {}
 
 float Student::getGpa() {
-  // TODO Fill in real implementation
-  return 0.0;
+  float sum = 0;
+	for (int i = 0; i <_grades.size();i++) {
+		sum = _grades[i] + sum;
+	}
+	return sum / _grades.size();
 }
 
 void Student::addGrade(float grade) {
-  // TODO Fill in real implementation
+	_grades.push_back(grade);
+	return;
 }
 
+std::string Student::majorString(Major m) {
+	switch (m) {
+		case Major::EE: 
+			return "EE";
+			break;
+		case Major::ME:
+			return "ME";
+			break;
+		case Major::CE:
+			return "CE";
+			break;
+		case Major::CHE:
+			return "CHE";
+			break;
+		case Major::BSE:
+			return "BSE";
+			break;
+		case Major::ART:
+			return "ART";
+			break;
+		case Major::ARCH:
+			return "ARCH";
+			break;
+	}
+		
+}
 const std::string& Student::getLastName() {
   // TODO Fill in real implementation
-  return std::string();
+  return _last_name;
 }
 
 void Student::printInfo() {
-  // TODO Fill in real implementation
-  printf("Student::printInfo() not implemented\n");
+ 	std::cout << _last_name << ", " << _first_name << std::endl;
+	std::cout << majorString(_major) <<" " <<_grad_year << std::endl;
+	std::cout << "GPA: " << std::fixed << std::setprecision(2) << getGpa() << std::endl;
+  	
 }
